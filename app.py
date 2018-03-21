48efc1e1b19bc1b18112c9060336e2b47d2424c2
成克彬 <chengkebin@shuidihuzhu.com>
2018-03-21 19:44:03 +0800

    add init data base

--- app.py
+++ app.py
@@ -2,7 +2,17 @@ from flask import Flask
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
 
 manager = Manager(app)
 
